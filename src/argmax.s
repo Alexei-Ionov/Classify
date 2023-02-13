37e694689ae938a2d461b0c58fa2d9821fa1216b
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-02-13 15:32:44 -0800

    task 3

--- src/argmax.s
+++ src/argmax.s
@@ -15,16 +15,31 @@
 #     this function terminates the program with error code 36
 # =================================================================
 argmax:
-    # Prologue
-
-
-loop_start:
-
-
-loop_continue:
-
-
-loop_end:
-    # Epilogue
-
+    addi t0 x0 1 ##creates a temp register with value of 1 to be used in the if statement below
+    mv t1 a0 #copy pointer over 
+    add t2 x0 x0 #initalizing start index
+    add t3 x0 x0 #initalizing res to index 0
+    add t4 x0 x0 #initalizing max value to be 0. 
+    bge a1 t0 Loop
+    li a0 36
+    j exit
+        
+Loop:
+    bge t2 a1 endLoop
+    lw t0 0(a0)
+    #bge t4 t0 Continue #if the curr max is greater than or equal to curr val, then just skip to the end of the block
+    jal ra Change_Max
+    addi t2 t2 1
+    addi t1 t1 4
+    j Loop
+    
+Change_Max: 
+    bge t4 t0 Continue
+    add t4 t0 x0 #update max register to equal the new max
+    add t3 t2 x0 #updating corresponding index 
+    j Continue
+    
+Continue: 
     jr ra
+
+endLoop:
