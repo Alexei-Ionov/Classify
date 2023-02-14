a8853bc24845658f84357a958df77a2e59872b4d
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-02-13 20:47:02 -0800

    made changes to dot.s (a2 was changed)

--- src/dot.s
+++ src/dot.s
@@ -19,33 +19,34 @@
 # =======================================================
 dot:
     addi t0 x0 1
-    mv t1 a0 #copy of arr_0 ptr
-    mv t2 a1 #copy of arr_1 ptr
+    mv t1 a0        #copy of arr_0 ptr
+    mv t2 a1        #copy of arr_1 ptr
 
-    add a0 x0 x0 #set res = 0
+    add a0 x0 x0    #set res = 0
 
-    addi t4 x0 4 #constant for next mul instruction. aka jump for each word
+    addi t4 x0 4    #constant for next mul instruction. aka jump for each word
 
-    mul t5 a3 t4 #new jump that considers stride for arr0
-    mul t6 a4 t4 #new jump that considers stride for arr1
+    mul t5 a3 t4    #new jump that considers stride for arr0
+    mul t6 a4 t4    #new jump that considers stride for arr1
 
     blt a2 t0 error_36 #less than one element to compare
     blt a3 t0 error_37
     blt a4 t0 error_37
+
+    add t0 x0 x0    #initialize counter instaed of using a2
     j Loop
     
 
 Loop:
-    beq a2 x0 quit   #used up all elements
+    bge t0 a2 quit   #used up all elements
     lw t3 0(t1)
     lw t4 0(t2)
-    mul t0 t3 t4 
-    add a0 a0 t0    #res += (t3 * t4)
+    mul t4 t3 t4 
+    add a0 a0 t4    #res += (t3 * t4)
     add t1 t1 t5    #ptr = ptr + (stride*4)
     add t2 t2 t6    #ptr = ptr + (stride*4)
     
-    addi t3 x0 1    #t3 = 1
-    sub a2 a2 t3    #essentially, a2 -= 1
+    addi t0 t0 1    #increment counter
     j Loop
 
 error_36: 
